35b418fe770fcbb5ff50952419830c996565150e
mgnamdan <danny.ai.hub@gmail.com>
2026-03-25 11:37:06 -0400

    update starter code

--- hangman.py
+++ hangman.py
@@ -15,18 +15,133 @@ def loadBank():
                 "chocolate", "astronaut"]
 
 
+def printMan(wrongGuesses):
+    
+    guesses = {0: noWrongGuesses(),
+               1: oneWrongGuess(),
+               2: twoWrongGuesses(),
+               3: threeWrongGuesses(),
+               4: fourWrongGuesses(),
+               5: fiveWrongGuesses(),
+               6: sixWrongGuesses(),
+               7: easterEgg()}
+
+    if wrongGuesses > 7 or wrongGuesses < 0:
+        wrongGuesses = 7
+
+    print(guesses[wrongGuesses])
+
+
+# ~~~~~ OUTPUT/DRAW FUNCTIONS ~~~~~
+def noWrongGuesses():
+    return """
+         +---+
+         |   |
+             |
+             |
+             |
+             |
+        ==========
+        """
+
+
+def oneWrongGuess():
+    return """
+         +---+
+         |   |
+         O   |
+             |
+             |
+             |
+        ==========
+        """
+
+
+def twoWrongGuesses():
+    return """
+         +---+
+         |   |
+         O   |
+         |   |
+             |
+             |
+        ==========
+        """
+
+
+def threeWrongGuesses():
+    return """
+         +---+
+         |   |
+         O   |
+        /|   |
+             |
+             |
+        ==========
+        """
+
+
+def fourWrongGuesses():
+    return """
+         +---+
+         |   |
+         O   |
+        /|\\  |
+             |
+             |
+        ==========
+        """
+
+
+def fiveWrongGuesses():
+    return """
+         +---+
+         |   |
+         O   |
+        /|\\  |
+        /    |
+             |
+        ==========
+        """
+
+
+def sixWrongGuesses():
+    return """
+         +---+
+         |   |
+         O   |
+        /|\\  |
+        / \\  |
+             |
+        ==========
+        """
+
+
+def easterEgg():
+    return """
+         +---+
+         |   |
+         O   |
+          /  |
+        /|   |
+        / \\  |
+        ==========
+        """
+
 
 # ~~~~~ MAIN DEFINITION ~~~~~
 def main():
-    # Load word bank
     wordBank = loadBank()
 
-    # Computer chooses a word
     choiceIdx = randint(0, (len(wordBank) - 1))
     chosenWord = wordBank[choiceIdx]
 
     print(chosenWord)
-    
+
+    # # For testing hangman print outputs
+    # for wrongGuessNum in range(8):
+    #     print("")
+    #     printMan(wrongGuessNum)
 
 
 # ~~~~~ MAIN CALL ~~~~~
